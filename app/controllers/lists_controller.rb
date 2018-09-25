7f653bd6fd702e39675421fa8332f3087c2c669f
Gennaro <g.anzalone12.com>
2018-09-25 15:26:37 -0700

    create and fill out lists controller

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,61 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      flash[:notice] = "List successfully added!"
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to lists_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_list
+      @list = List.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def task_params
+      params.require(:list).permit(:name)
+  end
+
+end
