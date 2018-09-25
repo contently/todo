caedf2a52433302e860c27a69d11e3df1df2facb
91juhwang <91juhwang@gmail.com>
2018-09-25 00:27:43 -0400

    Fix specs with updated codes and add more configs for rspec related gems

--- spec/features/tasks/create_spec.rb
+++ spec/features/tasks/create_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
+include TasksSpecSupport
 
 feature "Creating a task" do
+  let(:user) { create(:user) }
+  let(:list) { create(:list, user: user) }
+
+  before do
+    login_as user
+  end
+
   scenario "redirects to the tasks index page on success" do
-    visit tasks_path
-    click_on "Add a task"
+    visit list_tasks_path(list.id)
+    click_on "Create new task"
     expect(page).to have_content("Create a task")
 
     fill_in_form('Name', 'Test my app', 'Save')
@@ -13,7 +21,7 @@ feature "Creating a task" do
   end
 
   scenario "displays an error when no name is provided" do
-    visit new_task_path
+    visit new_list_task_path(list.id)
     fill_in_form('Name', '', 'Save')
 
     expect(page).to have_content("Name can't be blank")
