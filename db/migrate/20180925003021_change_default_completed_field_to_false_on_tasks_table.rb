9a7beea9abbdc6419db47de52595bb2b93f68e41
91juhwang <91juhwang@gmail.com>
2018-09-24 20:32:32 -0400

    Change default value of completed boolean field on tasks table

---
+++ db/migrate/20180925003021_change_default_completed_field_to_false_on_tasks_table.rb
@@ -0,0 +1,5 @@
+class ChangeDefaultCompletedFieldToFalseOnTasksTable < ActiveRecord::Migration[5.2]
+  def change
+    change_column :tasks, :completed, :boolean, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_25_001432) do
+ActiveRecord::Schema.define(version: 2018_09_25_003021) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
