9b7f3456669f6b58b397cdbb8589a233c7431918
91juhwang <91juhwang@gmail.com>
2018-09-24 22:09:48 -0400

    Add list_id to task_table

--- db/migrate/20180925012237_add_list_id_to_tasks_table_for_reference.rb
+++ db/migrate/20180925012237_add_list_id_to_tasks_table_for_reference.rb
@@ -1,5 +1,5 @@
 class AddListIdToTasksTableForReference < ActiveRecord::Migration[5.2]
   def change
-    add_reference :tasks, :list, index: true
+    add_reference :tasks, :list, index: true, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -25,8 +25,10 @@ ActiveRecord::Schema.define(version: 2018_09_25_012237) do
     t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "list_id_id"
     t.integer "list_id"
     t.index ["list_id"], name: "index_tasks_on_list_id"
+    t.index ["list_id_id"], name: "index_tasks_on_list_id_id"
   end
 
   create_table "users", force: :cascade do |t|
