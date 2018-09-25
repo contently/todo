7037f85ca82c88eeb073212946a2907831e750f7
Gennaro <g.anzalone12.com>
2018-09-25 12:28:00 -0700

    add scopes to separate completed from incompleted tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,12 @@
 class Task < ActiveRecord::Base
   validates :name, presence: true
+
+  scope(:not_completed, -> do
+    where({:completed => false})
+  end)
+
+  scope(:completed, -> do
+    where({:completed => true})
+  end)
 end
+
