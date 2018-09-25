098bdff9f0a3a215afdea9af1755cf00db579446
91juhwang <91juhwang@gmail.com>
2018-09-25 00:05:40 -0400

    Add FactoryBot fixtures

--- spec/factories/lists.rb
+++ spec/factories/lists.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :list do
-    
+    title 'Title'
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
-    
+    email 'test@test.com'
+    password '123456'
   end
 end
