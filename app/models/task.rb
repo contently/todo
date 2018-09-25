1b81b476b57f3b195ad8571a942dda4d71f73f0c
91juhwang <91juhwang@gmail.com>
2018-09-24 23:55:56 -0400

    Add #set_completed_field_to_false_for_new_records for setting default false values to completed field on task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,14 @@
 class Task < ActiveRecord::Base
+  before_save :set_completed_field_to_false_for_new_records
+
   validates :name, presence: true
+
   belongs_to :list
 
   scope :completed_tasks, -> { where completed: true }
   scope :incompleted_tasks, -> { where completed: false }
+
+  def set_completed_field_to_false_for_new_records
+    self.completed = false if self.new_record?
+  end
 end
