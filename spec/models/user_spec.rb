344ebb866d23988e901e4959f717ec6aa69aa6fb
Ramon Cabral <rrcm16@gmail.com>
2018-09-26 16:20:38 -0400

    Make user model validation test more specific

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,9 @@ describe User, type: :model do
   context "with no password" do
     it "is invalid" do
       user = User.new(username: 'testUser')
-      expect(user).to_not be_valid
+
+      expect(user.valid?).to be(false)
+      expect(user.errors.messages[:password_digest]).to include("can't be blank")
     end
   end
 end
