791a1c758d6930345eed2101fc93c4685e2462f9
Ramon Cabral <rrcm16@gmail.com>
2018-09-26 16:44:58 -0400

    Remove redundant test and use better selectors for link tests

--- spec/features/tasks/index_spec.rb
+++ spec/features/tasks/index_spec.rb
@@ -3,21 +3,14 @@ require 'rails_helper'
 feature 'Viewing all tasks' do
     scenario 'when visiting the page the first time, View All link is available' do
         visit tasks_path
-        expect(page).to have_content("View All")
+        
+        expect(page).to have_link("View All", href: tasks_path(all: true))
     end
 
     scenario 'when clicking View All, Incomplete tasks link is available' do
-        visit tasks_path
-        click_on 'View All'
-
-        expect(page).to have_content("Incomplete tasks")
-    end
-
-    scenario 'when clicking Incomplete tasks, View All link is available' do
         visit tasks_path(all: true)
-        click_on 'Incomplete tasks'
 
-        expect(page).to have_content("View All")
+        expect(page).to have_link("Incomplete tasks", href: tasks_path)
     end
     
 end
