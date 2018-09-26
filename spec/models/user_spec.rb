8a9947dde51f9dac3b841c42f62c9a708dfae09b
Ramon Cabral <rrcm16@gmail.com>
2018-09-26 15:54:33 -0400

    Add no password test for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 describe User, type: :model do
-  context "with no username" do
+  context "with no password" do
     it "is invalid" do
-      user = User.new
+      user = User.new(username: 'testUser')
       expect(user).to_not be_valid
     end
   end
