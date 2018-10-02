c4d56dc4b7f0d071f6464d09bf25e5015042d6e7
Saul <saulack@gmail.com>
2018-10-02 14:24:21 -0400

    Application Controler auth and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,35 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :logged_in?, :current_user
+
+
+  private
+
+  def login(user)
+    @current_user = user
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout
+    current_user.try(:reset_session_token!)
+    session[:session_token] = nil
+    @current_user = nil
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_login
+    redirect_to session_url unless logged_in?
+  end
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: "tasks#index"
   resources :tasks
+  resources :users, only: [:create]
+  resource :session, only: [:create, :destroy, :show]
 end
