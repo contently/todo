1ddd632d02b0f16ef18bc7fa8a74ff0842153150
Maria108 <maria.abash@gmail.com>
2018-10-02 21:55:27 -0400

    Adds helper method for device auth

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,8 +7,9 @@ require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
-require 'devise'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'devise'
+require_relative 'support/controller_macros' # or require_relative '../controller_macros' if write in `spec/support/devise.rb`
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -33,7 +34,11 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  config.include Devise::Test::ControllerHelpers, type: :controller
+  # For Devise > 4.1.1
+  config.include Devise::Test::ControllerHelpers, :type => :controller
+  # Use the following instead if you are on Devise <= 4.1.1
+  # config.include Devise::TestHelpers, :type => :controller
+  config.extend ControllerMacros, :type => :controller
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
