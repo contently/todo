b55b2a58f59e136c162166e8ae9ab43db5e114b3
Chris Haueter <chris.haueter@gmail.com>
2018-10-03 19:32:04 -0400

    Add task audit trail

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Task < ActiveRecord::Base
+  has_paper_trail
+
   validates :name, presence: true
-  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_26_152956) do
+ActiveRecord::Schema.define(version: 2018_10_03_220608) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.string "item_type", null: false
+    t.integer "item_id", null: false
+    t.string "event", null: false
+    t.string "whodunnit"
+    t.text "object", limit: 1073741823
+    t.datetime "created_at"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
 end
