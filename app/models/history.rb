bb258b5aee75c24589c2c7371e98f68853498db6
Kevin <kwang30@gmail.com>
2018-10-04 20:18:23 -0400

    added db changes with lists

---
+++ app/models/history.rb
@@ -0,0 +1,4 @@
+class History < ApplicationRecord
+  belongs_to :task
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :lists
+  resources :histories
   root to: 'tasks#index'
   resources :tasks
 end
