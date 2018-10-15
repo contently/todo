e2e8b20d54fa93db9ce472808a35c85441acefd8
Igor Eskin <eskinhd@gmail.com>
2018-10-14 21:26:33 -0400

    Added history to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_14_224019) do
+ActiveRecord::Schema.define(version: 2018_10_15_002022) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_10_14_224019) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.text "history"
   end
 
   create_table "users", force: :cascade do |t|
